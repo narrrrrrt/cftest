23fa2b17f7bef429292c6393c372abb6e7a4acff
narrrrrrt <222976023+narrrrrrt@users.noreply.github.com>
2025-09-07 09:59:29 +0900

    1

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 export default {
   async fetch(request: Request, env: any): Promise<Response> {
-    const url = new URL(request.url);
-    if (url.pathname === "/do") {
-      const id = env.ReversiDO.idFromName("global");
-      const stub = env.ReversiDO.get(id);
-      // クエリを維持して DO へフォワード
-      return stub.fetch(new Request(`http://do${url.pathname}${url.search}`, request));
-    }
-    return new Response("Not found", { status: 404 });
+    return await env.ASSETS.fetch(request);
   },
-};
-
-// Durable Object を再エクスポート
-export { ReversiDO } from "./ReversiDO";
+}
