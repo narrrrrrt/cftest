be00f745682daa69d4c9b1154f12114d053fb0f1
narrrrrrt <222976023+narrrrrrt@users.noreply.github.com>
2025-09-08 14:16:20 +0900

    0.5.1d

--- src/handlers/join.ts
+++ src/handlers/join.ts
@@ -40,6 +40,7 @@ export const joinAction: ActionHandler = async (params, ctx) => {
     role,
     token: genToken(),
     step: room.step,
+    (resp as any)._ = `${room.id},${seat},b=${+!!room.black},w=${+!!room.white},s=${room.step}`
   }
 
   return {
